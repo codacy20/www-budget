38cb7031382c375d95e7c04968af688fe83351db
codacy20 <kmuc72@gmail.com>
2019-08-02 00:09:40 +0200

    basic form. adding and removing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'www-budget';
+  expenseArray = []
+
+  addExpense(value) {
+    this.expenseArray.push(value);
+  }
+
+  deleteItem(value) {
+    for (let index = 0; index < this.expenseArray.length; index++) {
+      if (value == this.expenseArray[index]) {
+        this.expenseArray.splice(index, 1);
+      }
+    }
+  }
+
+  expenseSubmit(value: any) {
+    this.expenseArray.push(value.expense);
+    console.log(this.expenseArray);
+    
+  }
 }
