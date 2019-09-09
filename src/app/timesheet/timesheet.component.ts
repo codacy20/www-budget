ae10a169f70480290366fd81f3cecc59277bd76e
codacy20 <kmuc72@gmail.com>
2019-09-09 19:53:21 +0200

    timesheet data structure were adopted

--- src/app/timesheet/timesheet.component.ts
+++ src/app/timesheet/timesheet.component.ts
@@ -1,23 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Period } from '../Models/timesheet.interface';
+import { Timesheet } from '../Models/timesheet.interface';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+const ELEMENT_DATA: Period[] = [
+  {
+    finished: true,
+    start: new Date(),
+    end: new Date(),
+    hours: [
+      { hour: 2, date: new Date(), category: 'some project' },
+      { hour: 2, date: new Date(), category: 'some project' },
+      { hour: 2, date: new Date(), category: 'some project' },
+      { hour: 2, date: new Date(), category: 'some project' },
+    ],
+  },
 ];
 
 @Component({
@@ -26,12 +22,19 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./timesheet.component.css'],
 })
 export class TimesheetComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['hour', 'date', 'category'];
+  dataSource = ELEMENT_DATA[0].hours;
+  totalHours = 0;
+  activities = [];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.dataSource.forEach((element: Timesheet) => {
+      this.totalHours += element.hour;
+      this.activities.push(element.category);
+    });
+  }
 
   getDaysInMonth(month: number, year: number) {
     const date = new Date(month, year, 0).getDate();
