9ad0eb828544e362529d9a70ba1c9826667dca69
codacy20 <kmuc72@gmail.com>
2019-10-30 07:56:31 +0100

    TO-DO: issue with the init new period

--- src/app/timesheet/timesheet.service.ts
+++ src/app/timesheet/timesheet.service.ts
@@ -46,7 +46,7 @@ export class AppService {
       .pipe(catchError(this.errorHandler));
   }
 
-  async checkPeriod(date: Date, fetchPeriod: Period[]): Period {
+  checkPeriod(date: Date, fetchPeriod: Period[]): Period {
     let result: Period;
     fetchPeriod.forEach((element: Period) => {
       if (element.month === date.getMonth() + 1 && element.year === date.getFullYear()) {
